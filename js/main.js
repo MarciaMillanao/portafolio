14242fc45ed77338ce88e4b3064ab1778d0ae5c5
marcia <marcia.millanao@gmail.com>
2016-12-12 12:46:05 -0300

    arreglo iconos habilidades

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
 });
 
 
+$(document).ready(function(){  //habilidades
+	$('.carousel').carousel();
+});
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3);
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4);
+$('.carousel').carousel('set', 4);
